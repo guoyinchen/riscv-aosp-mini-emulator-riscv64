e8dcbe5be09f66bddccee0b91e9eaa5ffd0fe93e
guoyin.chen <guoyin.chen@gmail.com>
2020-09-11 22:07:10 +0800

    Init riscv 64 emulator device config
    Signed-off-by: guoyin.chen <guoyin.chen@gmail.com>

---
+++ mini_emulator_riscv64.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/generic/riscv64/mini_riscv64.mk)
+
+$(call inherit-product, device/generic/mini-emulator-armv7-a-neon/mini_emulator_common.mk)
+
+PRODUCT_NAME := mini_emulator_riscv64
+PRODUCT_DEVICE := mini-emulator-riscv64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := mini-riscv64
+
+LOCAL_KERNEL := prebuilts/qemu-kernel/rv64/kernel-qemu
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
